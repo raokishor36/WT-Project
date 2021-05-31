38cda7bc554e233536617d52d91fcf8343b34308
Kishor R Rao <43129439+raokishor36@users.noreply.github.com>
2021-05-31 20:14:03 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+import ProjectsSection from '../components/ProjectsSection';
+import TestimonialsSection from '../components/TestimonialsSection';
+import ContactBanner from '../components/ContactBanner';
+import HeroSection from '../components/HeroSection';
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+    </div>
+  );
+}
